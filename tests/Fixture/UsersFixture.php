be83e93036c66870d80ae6871ab6ccdecc3771b9
Tiago Marques <tpmarques03@gmail.com>
2025-02-27 03:50:56 +0000

    Adding the projetct with login working

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'created' => '2025-02-27 01:16:38',
+                'modified' => '2025-02-27 01:16:38',
+            ],
+        ];
+        parent::init();
+    }
+}
